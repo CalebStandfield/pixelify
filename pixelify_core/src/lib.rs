75c55983d1bf47e61428abb6e1217af91c93392f
CalebStandfield <54382087+CalebStandfield@users.noreply.github.com>
2025-12-14 23:09:23 -0700

    refactor of the library to move pixelify image structs to its own file

--- pixelify_core/src/lib.rs
+++ pixelify_core/src/lib.rs
@@ -1,27 +1,5 @@
+pub mod grayscale;
+pub mod pixelify_image;
+pub mod crop;
 
-
-pub struct PixelifyOptions {
-    pub width: u8,
-    pub height: u8
-}
-
-pub struct PixelifyImage {
-    pub pixels: Vec<u8>,
-    pub options: PixelifyOptions,
-}
-
-impl PixelifyImage {
-    pub fn new(bytes: Vec<u8>, height: u8, width: u8) -> PixelifyImage {
-        Self {
-            pixels: bytes,
-            options: PixelifyOptions {
-                width,
-                height,
-            }
-        }
-    }
-}
-
-pub fn pixelify_png(bytes: Vec<u8>, height: u8, width: u8) -> PixelifyImage {
-    PixelifyImage::new(bytes, height, width)
-}
+pub use pixelify_image::PixelifyImage;
---
+++ pixelify_core/src/pixelify_image.rs
@@ -0,0 +1,29 @@
+pub struct PixelifyOptions {
+    width: u32,
+    height: u32,
+}
+
+pub struct PixelifyImage {
+    pixels: Vec<u8>,
+    options: PixelifyOptions,
+}
+
+impl PixelifyImage {
+    pub fn new(bytes: Vec<u8>,width: u32,  height: u32) -> PixelifyImage {
+        Self {
+            pixels: bytes,
+            options: PixelifyOptions {
+                width,
+                height,
+            }
+        }
+    }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.pixels
+    }
+}
+
+pub fn pixelify_png(bytes: Vec<u8>, width: u32,  height: u32) -> PixelifyImage {
+    PixelifyImage::new(bytes, width, height)
+}
