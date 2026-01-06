8773673e853c773b2dbc9ecd4b8fbc78d902831e
CalebStandfield <54382087+CalebStandfield@users.noreply.github.com>
2026-01-06 10:42:43 -0700

    dithering file and lib pub mod statement
    # Conflicts:
    #	pixelify_core/src/lib.rs

---
+++ pixelify_core/src/dithering.rs
@@ -0,0 +1,12 @@
+//! A file that contains helper methods that dither an image.
+//! Dithering functions that us methods such as Bayer Matrix and Floyd-Steinberg will be available.
+
+use crate::pixelify_errors::ImageProcessingError;
+
+// pub fn bayer_matrix(bytes: &Vec<u8>) -> Result<Vec<u8>, ImageProcessingError> {
+//
+// }
+//
+// pub fn floyd_steinberg(bytes: &Vec<u8>) -> Result<Vec<u8>, ImageProcessingError> {
+//
+// }
--- pixelify_core/src/lib.rs
+++ pixelify_core/src/lib.rs
@@ -3,4 +3,5 @@ pub mod grayscale;
 pub mod pixelify;
 pub mod pixelify_errors;
 pub mod pixelify_image;
+pub mod dithering;
 pub use pixelify_image::PixelifyImage;
